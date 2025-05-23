268256c2b62c812c45e2e20c92136af9542b7da2
s.dayanithi <dayanithi360@gmail.com>
2025-05-23 08:22:03 +0530

    ch15

---
+++ Program Decision Making Statement java/demo21.java
@@ -0,0 +1,11 @@
+class demo21
+{
+    static int a=10;
+    static int b=20;
+    public static void main(String[]args)
+    {
+char res =(a>b)?'A':'B';
+    System.out.println(res+":is Grater");
+    }
+  
+}
